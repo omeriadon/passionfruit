857a2e80da609e80fe61acbfd397be6d71ac41ae
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-08-24 10:19:57 +0800

    connect account bookmarks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { RootProvider } from "fumadocs-ui/provider/next";
 import "./global.css";
 import { Inter } from "next/font/google";
+import { AuthProvider } from "@/lib/auth/AuthProvider";
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -10,7 +11,9 @@ export default function Layout({ children }: LayoutProps<"/">) {
 	return (
 		<html lang="en" className={inter.className} suppressHydrationWarning>
 			<body className="flex flex-col min-h-screen">
-				<RootProvider>{children}</RootProvider>
+				<AuthProvider>
+					<RootProvider>{children}</RootProvider>
+				</AuthProvider>
 			</body>
 		</html>
 	);
